8b07b29bc191611cafb9ba43564e28a4013d59c6
吴雪铭 <37106995+Xueming10wu@users.noreply.github.com>
2019-08-05 11:28:29 +0800

    Add files via upload
    写舵机

--- catkin_ws/src/bamboo/src/ArmProtocol.cpp
+++ catkin_ws/src/bamboo/src/ArmProtocol.cpp
@@ -1,59 +1,85 @@
 #include "bamboo/ArmProtocol.h"
 
+
 //构造函数
-ArmProtocol::ArmProtocol(){
+ArmProtocol::ArmProtocol():bufferSize(1024)
+{
+    rxBuffer = new uint8_t[bufferSize];
+    txBuffer = new uint8_t[bufferSize];
+    rxBuffer[bufferSize - 1] = '\0';
+    txBuffer[bufferSize - 1] = '\0';
+    KeyRX = 0;
+    KeyTX = 0;
     cout << "ArmProtocol construct" << endl;
 }
 
+
 //析构函数
-ArmProtocol::~ArmProtocol(){}
+ArmProtocol::~ArmProtocol()
+{
+    delete []txBuffer;
+    delete []rxBuffer;
+    txBuffer = NULL;
+    rxBuffer = NULL;
+}
 
 
 //读取某个舵机的脉宽(角度)，返回0则是读取数据失败
-int ArmProtocol::readJoint( int joint_id)
+int ArmProtocol::readJoint(Serial &serialport, int joint_id)
 {
-    //  #1PRAD\r\n
+    /////////////////////////////////////请求数据消息的创建////////////////////////////////////////
     stringstream ss;
-    ss << joint_id;
-    string joint_id_str = ss.str();
-    char *joint_id_chr = (char *)joint_id_str.c_str();
-    int id_len = strlen(joint_id_chr);
+    ss << "#";
+    ss << joint_id << "PRAD\r\n";
 
+    string txstring = ss.str();
+    char *tx_char = (char *)txstring.c_str();
+    KeyTX = strlen(tx_char);
 
-    char *request = new char[ 8 + id_len ];
-    request[ 8 + id_len - 1] = '\0';
-
-    request[0] = '#';
-    for (size_t i = 0; i < strlen(joint_id_chr); i++)
+    cout << "len " << KeyTX << " " << txstring;
+    for (size_t i = 0; i < KeyTX; i++)
     {
-        request[1 + i] = joint_id_chr[i];
+         txBuffer[i] = (uint8_t)tx_char[i];
     }
-    request[1 + id_len] = 'P';
-    request[2 + id_len] = 'R';
-    request[3 + id_len] = 'A';
-    request[4 + id_len] = 'D';
-    request[5 + id_len] = '\r';
-    request[6 + id_len] = '\n';
-    
-    cout << request << strlen(request) << endl;
-    //写入
-    //serialport.write()
+    /////////////////////////////////////请求数据消息的创建完毕//////////////////////////////////////
 
 
-    //响应
-    char *response = new char [11];
+    //////////////////////////////////////串口请求发送/////////////////////////////////////////////////
+    int txnum = serialport.write(txBuffer, KeyTX);
+    if (txnum != KeyTX)
+    {
+        cout << "readJoint write failed" << endl;
+        return 0;
+    }
+    else
+    {
+        cout << "readJoint write successed" << endl;
+    }
+
+    //////////////////////////////////////串口请求发送完毕/////////////////////////////////////////////
+
+
+    /////////////////////////////////////响应数据的接收//////////////////////////////////////////
+    //固定位数为11位
+    KeyRX = 11;
+
+    //rx索引
     int key = 0;
+    bool canRead = false;
     
-    while(true)
+    
+    while(!canRead)
     {   
-        //serialport.available() > 0
-        if (true)
+        //串口是否有数据
+        if (serialport.available())
         {
             //数据读取
-            //response[key] = (serialport.read(1).c_str())[0];
-            key ++;
-            if (response[0] != '#')
-            {//丢包错误
+            rxBuffer[key] = (serialport.read(1).c_str())[0];
+            key += 1;
+            if (rxBuffer[0] != (uint8_t)'#')
+            {   
+                //丢包错误
+                cout << "lost package:  #" << endl;
                 key = 0;
                 continue;
             }
@@ -62,31 +88,121 @@ int ArmProtocol::readJoint( int joint_id)
                 //正常执行时
                 continue;
             }
-            else
-            {   //当首位为#，并且key为11时，说明数据接收完毕
-                break;
+            if (rxBuffer[9] != (uint8_t)'\r')
+            {
+                cout << "lost package:  \\r" << endl;
+                key = 0;
+                continue;
             }
+            if (rxBuffer[10] != (uint8_t)'\n')
+            {
+                cout << "lost package:  \\n" << endl;
+                key = 0;
+                continue;
+            }
+            //通过检测，可以读取数据
+            canRead = true;
         }
         else
-        {
-            usleep(10);
+        {   
+            //等待数据
+            usleep(2000);
+            cout << "No data get" << endl;
         }
     }
 
-    //串口写入 串口读出
-    char *joint_res = new char[3];
-    char *position_res = new char[4];
-
-
+    cout << "RXBUFFER ";
+    for (size_t i = 0; i < KeyRX; i++)
+    {
+        cout << int(rxBuffer[i]) << " ";
+    }
+    cout << "\n\n";
     
 
+    
+    /////////////////////////////////////响应数据的接收完毕//////////////////////////////////////////
+
+
+
+    /////////////////////////////////////响应数据的解码////////////////////////////////////////////
+
+    //串口读出
+    int res_joint_id = int((rxBuffer[1] - uint8_t('0'))* 100 
+                    + (rxBuffer[2] - uint8_t('0')) * 10 
+                    + (rxBuffer[3] - uint8_t('0')) * 1);
+                    
+    int res_position = int((rxBuffer[5] - uint8_t('0') )* 1000 
+                    + (rxBuffer[6] - uint8_t('0') ) * 100 
+                    + (rxBuffer[7] - uint8_t('0')) * 10 
+                    + (rxBuffer[8] - uint8_t('0')) * 1);
+    /////////////////////////////////////响应数据的解码完毕//////////////////////////////////////////
+
+    if (res_joint_id == joint_id)
+    {
+        return res_position;
+    }
+    else
+    {
+        cout << "Read except joint is " << joint_id << " , but respond " << res_joint_id << " , position " << res_position << endl;
+    }
     
     return 0;
 }
 
-//写某个舵机的脉宽(角度)  返回是否成功 成功:1  失败:0
-int ArmProtocol::writeJoint(Serial& sp, int joint, int puls_width, int duration )
-{
 
-    return 0;
+//写某个舵机的脉宽(角度)  返回是否成功 成功:1  失败:0
+int ArmProtocol::writeJoint( Serial &serialport, int joint_id, int puls_width, int duration )
+{
+    stringstream ss;
+
+    ss << "#";
+    ss << joint_id << "P" << puls_width;
+    ss << "T" << duration << "\r\n" ;
+    string txstring = ss.str();
+
+    char *tx_char = (char *)txstring.c_str();
+    KeyTX = strlen(tx_char);
+    cout << "len " << KeyTX << " " << txstring;
+
+    for (size_t i = 0; i < KeyTX; i++)
+    {
+         txBuffer[i] = (uint8_t)tx_char[i];
+    }
+
+    int txnum = serialport.write(txBuffer, KeyTX);
+    if (txnum != KeyTX)
+    {
+        cout << "writeJoint write failed :" << endl;
+        return 0;
+    }
+    else
+    {
+        cout << "writeJoint write success !" << endl;
+    }
+    return 1;
+}
+
+
+void ArmProtocol::flushTxBuffer()
+{
+    for (size_t i = 0; i < bufferSize - 1; i++)
+    {
+        txBuffer[i] = '$';
+    }
+    KeyTX = 0;
+}
+
+void ArmProtocol::flushRxBuffer()
+{
+    for (size_t i = 0; i < bufferSize - 1; i++)
+    {
+        rxBuffer[i] = '$';
+    }
+    KeyRX = 0;
+}
+
+void ArmProtocol::flush()
+{
+    flushTxBuffer();
+    flushRxBuffer();
 }
--- catkin_ws/src/bamboo/src/arm_node.cpp
+++ catkin_ws/src/bamboo/src/arm_node.cpp
@@ -1,11 +1,58 @@
 #include "bamboo/ArmProtocol.h"
 
 
+//使用ctrl+C可以退出程序
+int quit_flag = 0;
+void exit_handler(int sig)
+{
+    quit_flag = 1;
+}
+
+//记录循环次数
+int loopcount = 0;
+
+
 int main(int argc, char *argv[])
 {
+    //按键捕获，用于退出while循环
+    signal(SIGINT,exit_handler);
+
+    //串口设置
+    int baudrate = 115200;
+    const string port = "/dev/ttyUSB1";
+    Timeout timeout = Timeout::simpleTimeout(1000);
+    Serial serialPort(port, baudrate, timeout);
+
+    cout << "Is the serial port open?";
+    if(serialPort.isOpen())
+    {
+        cout << " Yes." << endl;
+    }
+    else
+    {
+        cout << " No." << endl;
+        return 1;
+    }
+    
+
     //cout << "Hello arm" << endl;
     ArmProtocol protocol = ArmProtocol();
-    protocol.readJoint(263);
+
+    usleep(1000000);
+    //int position = protocol.readJoint(serialPort, 7);
+    //cout << "position is :" << position << endl;
+
+    protocol.writeJoint(serialPort, 7, 1500, 100);
+
+    /*
+    while (true)
+    {
+        int position = protocol.readJoint(serialPort, 263);
+        cout << "position is :" << position << endl;
+
+        usleep(1000000);
+    }
+    */
 
     return 0;
 }
